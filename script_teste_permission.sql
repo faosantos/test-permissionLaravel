a4f7a0515ce81379dae07d550d5bc7bc0c610119
Fernando Santos <faosantos@restinga.ifrs.edu.br>
2019-03-29 16:14:33 -0300

    create seeders

--- script_teste_permission.sql
+++ script_teste_permission.sql
@@ -2,3 +2,11 @@ create database teste_permission;
 use teste_permission;
 
 drop database teste_permission;
+
+SELECT * FROM teste_permission.users;
+SELECT * FROM teste_permission.roles;
+SELECT * FROM teste_permission.role_has_permissions;
+SELECT * FROM teste_permission.model_has_role;
+SELECT * FROM teste_permission.model_has_permissions;
+SELECT * FROM teste_permission.permissions;
+SELECT * FROM teste_permission.products;
